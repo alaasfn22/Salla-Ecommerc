028d319155759cf12c11abf9cd6d32a5824a4405
alaa-ali <aa0173422@gmail.com>
2024-02-01 13:15:13 +0200

    final change in website

--- src/redux/slice/CartSlice.jsx
+++ src/redux/slice/CartSlice.jsx
@@ -1,132 +1,130 @@
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import {useInsertData} from "../../Hook/useInsertData";
+import {useGetDataByToken} from "../../Hook/useGetData";
+import {useDeleteDataByToken} from "../../Hook/useDeleteData";
+import {useUpdateData} from "../../Hook/useUpdateData";
+export const addtoCart = createAsyncThunk(
+  "cart/addtoCart",
+  async (data, thunkAPI) => {
+    try {
+      const res = await useInsertData(`/api/v1/cart`, data);
+      return res;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.response);
+    }
+  }
+);
+export const getAllLoggedToCart = createAsyncThunk(
+  "cart/getAllLoggedToCart",
+  async (_, thunkAPI) => {
+    try {
+      const res = await useGetDataByToken(`/api/v1/cart`);
+      return res;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.response);
+    }
+  }
+);
+export const rempveSpecificProduct = createAsyncThunk(
+  "cart/rempveSpecificProduct",
+  async (id, thunkAPI) => {
+    try {
+      const res = await useDeleteDataByToken(`/api/v1/cart/${id}`);
+      return res;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.response);
+    }
+  }
+);
+export const removeAll = createAsyncThunk(
+  "cart/removeAll",
+  async (_, thunkAPI) => {
+    try {
+      const res = await useDeleteDataByToken(`/api/v1/cart`);
+      return res;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.response);
+    }
+  }
+);
+export const updateCartCount = createAsyncThunk(
+  "cart/updateCartCount",
+  async (data, thunkAPI) => {
+    try {
+      const res = await useUpdateData(`/api/v1/cart/${data.id}`, data);
 
-import {createAsyncThunk, createSlice} from "@reduxjs/toolkit"
-import { useInsertData } from "../../Hook/useInsertData"
-import { useGetDataByToken } from "../../Hook/useGetData"
-import { useDeleteDataByToken } from "../../Hook/useDeleteData"
-import { useUpdateData } from "../../Hook/useUpdateData"
-export const addtoCart=createAsyncThunk(
-    "cart/addtoCart",
-    async(data,thunkAPI)=>{
-        try{
-            const res=await useInsertData(`/api/v1/cart`, data)
-           return res  
-        }catch(e){
-            return thunkAPI.rejectWithValue(e.response)
-        }
+      return res;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.response);
     }
-)
-export const getAllLoggedToCart=createAsyncThunk(
-    "cart/getAllLoggedToCart",
-    async(_,thunkAPI)=>{
-        try{
-            const res=await useGetDataByToken(`/api/v1/cart`)
-           return res  
-        }catch(e){
-            return thunkAPI.rejectWithValue(e.response)
-        }
-    }
-)
-export const rempveSpecificProduct=createAsyncThunk(
-    "cart/rempveSpecificProduct",
-    async(id,thunkAPI)=>{
-        try{
-            const res=await useDeleteDataByToken(`/api/v1/cart/${id}`)
-           return res  
-        }catch(e){
-            return thunkAPI.rejectWithValue(e.response)
-        }
-    }
-)
-export const removeAll=createAsyncThunk(
-    "cart/removeAll",
-    async(_,thunkAPI)=>{
-        try{
-            const res=await useDeleteDataByToken(`/api/v1/cart`)
-           return res  
-        }catch(e){
-            return thunkAPI.rejectWithValue(e.response)
-        }
-    }
-)
-export const updateCartCount=createAsyncThunk(
-    "cart/updateCartCount",
-    async(data,thunkAPI)=>{
-        try{
-            const res=await useUpdateData(`/api/v1/cart/${data.id}`, data)
+  }
+);
 
-           return res  
-        }catch(e){
-            return thunkAPI.rejectWithValue(e.response)
-        }
-    }
-)
-
-const initialState={cart:[],cupon:[],isLoading:false,error:null}
-const cartSlice=createSlice({
-    name:"cart",
-    initialState,
-    extraReducers:{
-        [addtoCart.pending]: (state) => {
-            state.isLoading = true;
-            state.error = null;
-        },
-        [addtoCart.fulfilled]: (state, action) => {
-            state.cart = action.payload;
-            state.isLoading = false;
-        },
-        [addtoCart.rejected]: (state, action) => {
-            state.error = action?.payload;
-            state.isLoading = false;
-        },
-        [getAllLoggedToCart.pending]: (state) => {
-            state.isLoading = true;
-            state.error = null;
-        },
-        [getAllLoggedToCart.fulfilled]: (state, action) => {
-            state.cart = action?.payload;
-            state.isLoading = false;
-        },
-        [getAllLoggedToCart.rejected]: (state, action) => {
-            state.error = action?.payload;
-            state.isLoading = false;
-        },
-        [rempveSpecificProduct.pending]: (state) => {
-            state.isLoading = true;
-            state.error = null;
-        },
-        [rempveSpecificProduct.fulfilled]: (state, action) => {
-            state.cart = action?.payload;
-            state.isLoading = false;
-        },
-        [rempveSpecificProduct.rejected]: (state, action) => {
-            state.error = action?.payload;
-            state.isLoading = false;
-        },
-        [removeAll.pending]: (state) => {
-            state.isLoading = true;
-            state.error = null;
-        },
-        [removeAll.fulfilled]: (state, action) => {
-            state.cart = action.payload;
-            state.isLoading = false;
-        },
-        [removeAll.rejected]: (state, action) => {
-            state.error = action?.payload;
-            state.isLoading = false;
-        },
-        [updateCartCount.pending]: (state) => {
-            state.isLoading = true;
-            state.error = null;
-        },
-        [updateCartCount.fulfilled]: (state, action) => {
-            state.cart = action.payload;
-            state.isLoading = false;
-        },
-        [updateCartCount.rejected]: (state, action) => {
-            state.error = action?.payload;
-            state.isLoading = false;
-        }
-    }
-    
-})
-export default cartSlice.reducer
+const initialState = {cart: [], cupon: [], isLoading: false, error: null};
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  extraReducers: {
+    [addtoCart.pending]: (state) => {
+      state.isLoading = true;
+      state.error = null;
+    },
+    [addtoCart.fulfilled]: (state, action) => {
+      state.cart = action.payload;
+      state.isLoading = false;
+    },
+    [addtoCart.rejected]: (state, action) => {
+      state.error = action?.payload;
+      state.isLoading = false;
+    },
+    [getAllLoggedToCart.pending]: (state) => {
+      state.isLoading = true;
+      state.error = null;
+    },
+    [getAllLoggedToCart.fulfilled]: (state, action) => {
+      state.cart = action?.payload;
+      state.isLoading = false;
+    },
+    [getAllLoggedToCart.rejected]: (state, action) => {
+      state.error = action?.payload;
+      state.isLoading = false;
+    },
+    [rempveSpecificProduct.pending]: (state) => {
+      state.isLoading = true;
+      state.error = null;
+    },
+    [rempveSpecificProduct.fulfilled]: (state, action) => {
+      state.cart = action?.payload;
+      state.isLoading = false;
+    },
+    [rempveSpecificProduct.rejected]: (state, action) => {
+      state.error = action?.payload;
+      state.isLoading = false;
+    },
+    [removeAll.pending]: (state) => {
+      state.isLoading = true;
+      state.error = null;
+    },
+    [removeAll.fulfilled]: (state, action) => {
+      state.cart = action.payload;
+      state.isLoading = false;
+    },
+    [removeAll.rejected]: (state, action) => {
+      state.error = action?.payload;
+      state.isLoading = false;
+    },
+    [updateCartCount.pending]: (state) => {
+      state.isLoading = true;
+      state.error = null;
+    },
+    [updateCartCount.fulfilled]: (state, action) => {
+      state.cart = action.payload;
+      state.isLoading = false;
+    },
+    [updateCartCount.rejected]: (state, action) => {
+      state.error = action?.payload;
+      state.isLoading = false;
+    },
+  },
+});
+export default cartSlice.reducer;
